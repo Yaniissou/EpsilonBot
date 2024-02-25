395ffa264fb616b96fa34fb50bf7309031e0b50f
Yanissou <yanissoumc@gmail.com>
2024-02-25 13:25:37 +0100

    Testings ids + db debug

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,9 @@ import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -13,14 +16,33 @@ public class Main {
 
     public static long SERVER_ID = 1196782142739447858L;
 
-    public static long ANNOUNCE_CHANNEL_ID = 1196782376269918299L;
-    public static long LOGS_CHANNEL_ID = 1201545255024087161L;
-    public static List<Long> WHITELISTED_IDS = Arrays.asList(262674203931574273L, 191213751746166785L);
-    public static long BOT_ID = 1209564326902366249L;
-    public static List<Long> ROLES_TO_PING = Arrays.asList(1196782266861502484L, 1209253560445960243L);
+    // public static long ANNOUNCE_CHANNEL_ID = 1196782376269918299L;
+    //TESTING :
+    public static long ANNOUNCE_CHANNEL_ID = 1209564850142056531L;
 
+    //public static long LOGS_CHANNEL_ID = 1201545255024087161L;
+    //TESTING :
+    public static long LOGS_CHANNEL_ID = 1209580226024317008L;
+    public static List<Long> WHITELISTED_IDS = Arrays.asList(262674203931574273L, 191213751746166785L);
+    //public static long BOT_ID = 1209564326902366249L;
+    //TESTING :
+    public static long BOT_ID = 1209564326902366249L;
+
+    //public static List<Long> ROLES_TO_PING = Arrays.asList(1196782266861502484L, 1209253560445960243L);
+    //TESTING :
+    public static List<Long> ROLES_TO_PING = Arrays.asList(1209593119675387974L);
     public static void main(String[] args) {
 
+        // Chemin absolu vers la base de données SQLite
+        String dbPath = "/chemin/vers/votre/base/de/donnees.db";
+
+        // Connexion à la base de données SQLite
+        try (Connection connection = DriverManager.getConnection("jdbc:sqlite:" + dbPath)) {
+            // Effectuer des opérations sur la base de données
+            System.out.println("Connexion à la base de données SQLite réussie.");
+        } catch (SQLException e) {
+            System.err.println("Erreur lors de la connexion à la base de données SQLite : " + e.getMessage());
+        }
 
         JDABuilder builder = JDABuilder.createDefault(args[0]);
         builder.enableIntents(GatewayIntent.GUILD_MESSAGE_REACTIONS);
