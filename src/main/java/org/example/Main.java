4c48a22dcdeb60b904c463c5b6d46faa3b33327b
Yanissou <yanissoumc@gmail.com>
2024-02-25 16:57:59 +0100

    Revert "risky debug"
    This reverts commit ff7c71e5ced28abc7052c2ff87bb57163e9b2bd4.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -34,7 +34,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("TESTING HERE");
-        System.out.println(args[0]);
         // Chemin absolu vers la base de données SQLite
         String dbPath = "/app/db/botDB.db";
 
