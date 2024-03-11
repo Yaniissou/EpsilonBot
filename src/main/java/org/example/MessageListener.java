0507b324ff67ad3d098dd48859de60c04b87be14
yanis <yanissoumc@gmail.com>
2024-03-11 15:59:19 +0100

    added quoicoubeh interaction

--- src/main/java/org/example/MessageListener.java
+++ src/main/java/org/example/MessageListener.java
@@ -5,13 +5,16 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Random;
+
 public class MessageListener extends ListenerAdapter {
 
 
     @Override
     public void onMessageReceived(@NotNull MessageReceivedEvent event){
         if (event.getMessage().getContentRaw().endsWith("quoi")){
-            event.getChannel().sendMessage("feur").queue();
+            (new Random().nextInt(2) == 1 ? event.getChannel().sendMessage("feur").queue() : event.getChannel().sendMessage("coubeh").queue();)
+
         }
     }
 }
