38bb0995a137b98230667ecdb44305db390e43d4
Yanissou <yanissoumc@gmail.com>
2024-08-20 21:05:50 +0200

    try another update

--- src/main/java/org/example/MessageListener.java
+++ src/main/java/org/example/MessageListener.java
@@ -15,7 +15,6 @@ public class MessageListener extends ListenerAdapter {
         if (event.getMessage().getContentRaw().endsWith("quoi")){
              event.getChannel().sendMessage(new Random().nextInt(2) == 1 ? "feur" : "coubeh").queue();
         } else if (event.getMessage().getAuthor().getId().equalsIgnoreCase("639734043906408448")){
-            event.getChannel().sendMessage("Monkey").queue();
             event.getMessage().addReaction(Emoji.fromUnicode("\uD83D\uDC35")).queue();
         }
     }
