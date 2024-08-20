c6e3b8a144f85b75c913be00c4bc90db9c4971c8
Yanissou <yanissoumc@gmail.com>
2024-08-20 20:42:52 +0200

    Test to implement auto container reboot

--- src/main/java/org/example/MessageListener.java
+++ src/main/java/org/example/MessageListener.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import net.dv8tion.jda.api.entities.emoji.Emoji;
 import net.dv8tion.jda.api.events.message.GenericMessageEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -14,7 +15,8 @@ public class MessageListener extends ListenerAdapter {
     public void onMessageReceived(@NotNull MessageReceivedEvent event){
         if (event.getMessage().getContentRaw().endsWith("quoi")){
              event.getChannel().sendMessage(new Random().nextInt(2) == 1 ? "feur" : "coubeh").queue();
-
+        } else if (event.getMessage().getAuthor().getId().equalsIgnoreCase("639734043906408448")){
+            event.getMessage().addReaction(Emoji.fromUnicode("\uD83D\uDC35")).queue();
         }
     }
 }
